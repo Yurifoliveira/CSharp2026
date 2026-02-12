b46b80f53e9238161329f3ab53c71b015697389e
Yurifoliveira <yuri.fyo2@outlook.com>
2026-02-11 21:25:16 -0300

    primeiro código

---
+++ projeto.cs
@@ -0,0 +1,9 @@
+using System; //biblioteca do c# (system) para usar comandos do console
+public class Program
+{
+    public static void Main ()
+    {
+        Console.WriteLine("Senai");
+    }
+}
+
