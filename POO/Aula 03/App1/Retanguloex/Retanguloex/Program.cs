590ee0f2eef677a64f8c4010546cf1a47a5a39c8
Yurifoliveira <yuri.fyo2@outlook.com>
2026-03-25 18:34:18 -0300

    retangulo exercicio

---
+++ POO/Aula 03/App1/Retanguloex/Retanguloex/Program.cs
@@ -0,0 +1,12 @@
+﻿using Retanguloex;
+using static System.Console;
+Retangulo x = new Retangulo();
+//entrad dados
+WriteLine("digite as medidas retangulo 1 : ");
+
+Write("digite a largura  :");
+x.largura = double.Parse(ReadLine());
+
+Write("digite a altura  : ");
+x.altura = double.Parse(ReadLine());
+x.Process();
---
+++ POO/Aula 03/App1/Retanguloex/Retanguloex/Retangulo.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+namespace Retanguloex;
+
+internal class Retangulo
+{
+    public double altura;
+    public double largura;
+
+
+
+
+
+    public void Process()
+    {
+        double area = (altura * largura);
+        double p = ((2 * altura) + (2 * largura));
+        double diagonal = Math.Sqrt((largura * largura) + (altura * altura));
+        WriteLine($"a area é  : {area} , o perimetro é de : {p} e a diagonal é de : {diagonal} ");
+
+
+    }
+
+}
