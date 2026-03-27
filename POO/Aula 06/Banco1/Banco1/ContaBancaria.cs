62b37933a69ca2c974a561cdd2dcc34f9ec835c9
Yurifoliveira <yuri.fyo2@outlook.com>
2026-03-27 20:56:05 -0300

    Aula 6 , 27/03

---
+++ POO/Aula 06/Banco1/Banco1/ContaBancaria.cs
@@ -0,0 +1,76 @@
+﻿using static System.Console;
+using System;
+namespace Banco1
+{
+	internal class ContaBancaria
+	{
+		//campo
+
+		private double saldo;
+		private string titular;
+		private int numero;
+
+		//propriedades
+
+		public int Numero
+		{
+			get { return numero; }
+			set { numero = value; }
+		}
+
+		public string Titular
+		{
+			get { return titular; }
+			set { titular = value; }
+		}
+
+
+		public double Saldo
+		{
+			get { return saldo; }
+			set
+			{
+				if (value > 0)
+				{
+					saldo = value;
+				}
+				else
+				{
+					saldo = 0;
+					Write("Saldo negativo invalido");
+				}
+			}
+		}
+			
+		//construtores ContaBancaria
+		public ContaBancaria(int numero, string titular)
+		{
+			Numero = numero;
+			Titular = titular;
+			Saldo = 0;
+		}
+
+		// herança do construtor
+		public ContaBancaria(int numero, string titular, double saldo) : this(numero, titular)
+		{
+
+			Saldo = saldo;
+
+		}
+		//metodos
+		public void Deposito(double quantia)
+		{
+			Saldo += quantia;
+		}
+
+		public void Saque(double quantia)
+		{
+			Saldo -= quantia;
+		}
+	    public void Dados()
+		{
+            Console.WriteLine($"dados da conta :\n "+ $"\tConta : {Numero}\n"+$"\tTitular : {Titular}\n"+$"Saldo : {Saldo:C}");
+		}
+    }
+}
+
---
+++ POO/Aula 06/Banco1/Banco1/Program.cs
@@ -0,0 +1,23 @@
+﻿using Banco1;
+using static System.Console;
+Title = "Cadastro de conta:";
+ContaBancaria conta;
+//entrada de dados
+Write("entre com o numero da conta");
+int n = int.Parse(ReadLine());
+Write("entre com o nome do titular da conta :");
+string nome = ReadLine();//ReadLIne vazio para ler texto
+Write("Deseja fazer um deposito inicial (s / n) :");
+char resposta = char.Parse(ReadLine());
+if (resposta == 's')
+{
+    Write("entre com o valor do deposito inicial : ");
+    double dep = double.Parse(ReadLine());
+    conta = new ContaBancaria(n, nome, dep); //criação de objt
+}
+else
+{
+    conta = new(n, nome);//criaçao de objt
+}
+conta.Dados();
+ReadKey();
